5e8cf2f19843051bb1596603a0398dd168087dd0
JayPi4c <32594337+JayPi4c@users.noreply.github.com>
2017-10-29 16:54:35 +0100

    added the main idea of this sketch
    The main Idea is that there is a population which reacts to the given lifetime and evaluates a better dna from the previous generation.
    Every 50 generations there will be a save of the curent dna in an external file such as .txt.

--- Main.ino
+++ Main.ino
@@ -1,9 +1,29 @@
-#inlcude "Vector.h"
+#include "Population.h"
+
+int popsize = 200;
+int generation = 1;
+int lifetime = 500;
+int count;
+
+
+Population p(popsize);
 
 void setup(){
-
+  count = 0;
 }
 
 void loop(){
-
+  count = ++count % lifetime;
+  if(count == 0 || p.isDead()){
+    count = 0;
+    p.evaluate();
+    p.selection();
+    generation++;
+  }
+  p.run();
+  
+  if(generation % 50 == 0){
+    //write the DNAs in en external file to save the current DNA state
+    //this should allow the user to get an better overview of the generation
+  }
 }
